d4952892ef8c204d75031b1a65fd6682b8e20a6f
sami <samisabir212@gmail.com>
2017-06-01 14:47:48 -0400

    from mac

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+//        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:/Volumes/Production/Courses/Programs/JavaPrograms/TestDB/testjava.db");
+//            Statement statement = conn.createStatement()) {
+//            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Volumes/Production/Courses/Programs/JavaPrograms/TestDB/testjava.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+
+            statement.close();
+            conn.close();
+
+//            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\databases\\testjava.db");
+//            Class.forName("org.sql.JDBC");
+
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
     }
 }
